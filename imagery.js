3ac379434aa997a7d4e7a42e9bac050bf250eb64
LORD KAY <offeilord@gmail.com>
2017-08-08 12:43:24 +0000

    Opacity workin perfect -- Trying different idea but not working for now

--- imagery.js
+++ imagery.js
@@ -27,51 +27,7 @@
 			 }
 		}
 
-		//Optimization by function creations - Don't Ask
-		function _OPACRY(){
-			var _OPVALUE = settings.addOpacity;
-			switch(_OPVALUE)
-			{
-				case 0.2 || .2:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[0]);
-					break;
-				case 0.3 || .3:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[1]);
-					break;
-				case 0.4 || .4:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[2]);
-					break;
-				case 0.5 || .5:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[3]);
-					break;
-				case 0.6 || .6:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[4]);
-					break;
-				case 0.7 || .7:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[5]);
-					break;
-				case 0.8 || .8:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[6]);
-					break;
-				case 0.9 || .9:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[7]);
-
-				default:
-					$(".indicator").css("opacity",opaqueArray[8]);
-					$(this).css("opacity",opaqueArray[8]);
-					break;
-			}
-		}
-
-		$Imagery.each(function(){
+		$.each($Imagery,function(){
 			//implementing the click functionality for the images
 				$Imagery.addClass("indicator");
 
@@ -79,7 +35,46 @@
 					//Defining an array for the opacity values
 					var opaqueArray = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
 					//Calling the opacity functionality to do its work --> Sharp
-					_OPACRY();
+					var _OPVALUE = settings.addOpacity;
+					switch(_OPVALUE)
+					{
+						case 0.2 || .2:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[0]);
+							break;
+						case 0.3 || .3:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[1]);
+							break;
+						case 0.4 || .4:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[2]);
+							break;
+						case 0.5 || .5:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[3]);
+							break;
+						case 0.6 || .6:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[4]);
+							break;
+						case 0.7 || .7:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[5]);
+							break;
+						case 0.8 || .8:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[6]);
+							break;
+						case 0.9 || .9:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[7]);
+
+						default:
+							$(".indicator").css("opacity",opaqueArray[8]);
+							$(this).css("opacity",opaqueArray[8]);
+							break;
+					}
 					//Calling the indicator operation here
 					if (settings.indicator && settings.indicator == false) 
 					{
